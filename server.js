6de679a19fdf33e28b2ed02dab11f530b2806beb
vivek shukla <vivekshukla@viveks-MacBook-Air.local>
2019-01-18 23:22:20 +0530

    API setting up

--- server.js
+++ server.js
@@ -1,89 +1,34 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mysql = require('mysql');
-/*var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'dbuser',
-  password : 's3kreee7',
-  database : 'my_db'
-});*/
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 
-var CONTACTS_COLLECTION = "contacts";
+var apiRouter = require('./api/routes/apiRoutes');
 
 var app = express();
-app.use(bodyParser.json());
 
-// Create link to Angular build directory
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'dist/eazytaxin')));
+app.use('/', express.static(path.join(__dirname, 'dist/eazytaxin')));
+app.use('/api', apiRouter);
 
-/*connection.connect();
-// Connect to the database before starting the application server.
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-  if (err) throw err
-
-  console.log('The solution is: ', rows[0].solution)
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-connection.end();*/
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// Initialize the app.
-var server = app.listen(process.env.PORT || 3000, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
+  // render the error page
+  res.status(err.status || 500);
+  res.send(err.status);
 });
 
-// CONTACTS API ROUTES BELOW
-
-// Generic error handler used by all endpoints.
-function handleError(res, reason, message, code) {
-    console.log("ERROR: " + reason);
-    res.status(code || 500).json({"error": message});
-  }
-  
-  /*  "/api/contacts"
-   *    GET: finds all contacts
-   *    POST: creates a new contact
-   */
-  
-  app.get("/api/contacts", function(req, res) {
-    db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
-      if (err) {
-        handleError(res, err.message, "Failed to get contacts.");
-      } else {
-        res.status(200).json(docs);
-      }
-    });
-  });
-  
-  app.post("/api/contacts", function(req, res) {
-    var newContact = req.body;
-    newContact.createDate = new Date();
-  
-    if (!req.body.name) {
-      handleError(res, "Invalid user input", "Must provide a name.", 400);
-    } else {
-      db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
-        if (err) {
-          handleError(res, err.message, "Failed to create new contact.");
-        } else {
-          res.status(201).json(doc.ops[0]);
-        }
-      });
-    }
-  });
-  
-  /*  "/api/contacts/:id"
-   *    GET: find contact by id
-   *    PUT: update contact by id
-   *    DELETE: deletes contact by id
-   */
-  
-  app.get("/api/contacts/:id", function(req, res) {
-  });
-  
-  app.put("/api/contacts/:id", function(req, res) {
-  });
-  
-  app.delete("/api/contacts/:id", function(req, res) {
-  });
+module.exports = app;
