56aa9900d53a32bdd7f8718940a7149ce75cd272
hardik bhaliya <hardy2899@gmail.com>
2019-01-29 00:52:31 +0530

    forms can be changed by angular variable way.

--- src/app/taxfilling/personal-details/personal-details.component.ts
+++ src/app/taxfilling/personal-details/personal-details.component.ts
@@ -10,9 +10,242 @@ import { Basicinfostep1, Personalinfo, Address, Bankdetail, Assetsliabilities } 
   styleUrls: ['./personal-details.component.css']
 })
 export class PersonalDetailsComponent implements OnInit {
+  registerForm1: FormGroup;
+  loading = false;
+  submitted = false;
+  step1:boolean = true;
+  step2:boolean = false;
+  step3:boolean = false;
+  step4:boolean = false;
+  step5:boolean = false;
+  step4_primary_bank_acc:boolean = true;
+  step4_all_other_bank_acc:boolean = false;
+  step5_immovable:boolean = true;
+  step5_movable:boolean = false;
+  step5_foreign:boolean = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+    ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    // this.registerForm1 = this.formBuilder.group({
+    //   emailId: ['', Validators.required ],
+    //   nPassword: ['', Validators.required ],
+    //   cPassword: ['', Validators.required],
+    //   terms: ['', Validators.required]
+    // });
+  }
+  get f() { return this.registerForm1.controls; }
 
+  onSubmit() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.registerForm1.invalid) {
+          return;
+      }
+
+      this.loading = true;
+      console.log('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm1.value))
+      // this.userService.register(this.registerForm.value)
+      //     .pipe(first())
+      //     .subscribe(
+      //         data => {
+      //             this.alertService.success('Registration successful', true);
+      //             this.router.navigate(['/login']);
+      //         },
+      //         error => {
+      //             this.alertService.error(error);
+      //             this.loading = false;
+      //         });
+  }
+  on_next_click(){
+    if (this.step5 == true) {
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = false;
+      this.step5 = true;
+    }
+    if (this.step4 == true) {
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = false;
+      this.step5 = true;
+    }
+    if (this.step3 == true) {
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = true;
+      this.step5 = false;
+    }
+    if (this.step2 == true) {
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = true;
+      this.step4 = false;
+      this.step5 = false;
+    }
+    if (this.step1 == true) {
+      this.step1 = false;
+      this.step2 = true;
+      this.step3 = false;
+      this.step4 = false;
+      this.step5 = false;
+    }
+  }
+  on_previous_click(){
+    if (this.step1 == true) {
+      this.step1 = true;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = false;
+      this.step5 = false;
+    }
+    if (this.step2 == true) {
+      this.step1 = true;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = false;
+      this.step5 = false;
+    }
+    if (this.step3 == true) {
+      this.step1 = false;
+      this.step2 = true;
+      this.step3 = false;
+      this.step4 = false;
+      this.step5 = false;
+    }
+    if (this.step4 == true) {
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = true;
+      this.step4 = false;
+      this.step5 = false;
+    }
+    if (this.step5 == true) {
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = true;
+      this.step5 = false;
+    }
+  }
+  on_skip_click(){
+    if (this.step5 == true) {
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = false;
+      this.step5 = true;
+    }
+    if (this.step4 == true) {
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = false;
+      this.step5 = true;
+    }
+    if (this.step3 == true) {
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = true;
+      this.step5 = false;
+    }
+    if (this.step2 == true) {
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = true;
+      this.step4 = false;
+      this.step5 = false;
+    }
+    if (this.step1 == true) {
+      this.step1 = false;
+      this.step2 = true;
+      this.step3 = false;
+      this.step4 = false;
+      this.step5 = false;
+    }
+  }
+  on_reset_click(){
+    // if (this.step1 == true) {
+    // }
+    // if (this.step2 == true) {
+    // }
+    // if (this.step3 == true) {
+    // }
+    // if (this.step4 == true) {
+    // }
+    // if (this.step5 == true) {
+    // }
+  }
+  select_form_step(a){
+    if(a == 'step1'){
+      this.step1 = true;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = false;
+      this.step5 = false;
+    }
+    if(a == 'step2'){
+      this.step1 = false;
+      this.step2 = true;
+      this.step3 = false;
+      this.step4 = false;
+      this.step5 = false;
+    }
+    if(a == 'step3'){
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = true;
+      this.step4 = false;
+      this.step5 = false;
+    }
+    if(a == 'step4'){
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = true;
+      this.step5 = false;
+    }
+    if(a == 'step5'){
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = false;
+      this.step5 = true;
+    }
+  }
+  select_step5_subform_part(x){
+    if( x == 'immovable') {
+      this.step5_immovable = true;
+      this.step5_movable = false;
+      this.step5_foreign = false;
+    }
+    if( x == 'movable') {
+      this.step5_immovable = false;
+      this.step5_movable = true;
+      this.step5_foreign = false;
+    }
+    if( x == 'foreign') {
+      this.step5_immovable = false;
+      this.step5_movable = false;
+      this.step5_foreign = true;
+    }
+  }
+  select_step4_subpart_form(z){
+    if( z == 'Primary_Bank_Account') {
+      this.step4_primary_bank_acc = true;
+      this.step4_all_other_bank_acc = false;
+    }
+    if( z == 'All_Other_Bank_Account') {
+      this.step4_primary_bank_acc = false;
+      this.step4_all_other_bank_acc = true;
+    }
+  }
 }
