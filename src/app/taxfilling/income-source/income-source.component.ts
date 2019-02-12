cd2e3010c3abb175307ee08701c9097f68ee3e6f
Vivek Shukla <viveksh970@IN.nam.ad.pwcinternal.com>
2019-02-12 16:47:22 +0530

    taxfilling and JS init changes-ByVIveks

--- src/app/taxfilling/income-source/income-source.component.ts
+++ src/app/taxfilling/income-source/income-source.component.ts
@@ -8,6 +8,30 @@ import { ScriptService } from '@app/_services';
   styleUrls: ['./income-source.component.css']
 })
 export class IncomeSourceComponent implements OnInit {
+  step1:boolean = true;
+  step2:boolean = false;
+  step3:boolean = false;
+  step4:boolean = false;
+
+  step1_income_salary_upload :boolean = true;
+  step1_income_from_salary:boolean = false;
+  
+  step2_income_other_upload :boolean = true;
+  step2_income_interest:boolean = false;
+  step2_income_others:boolean = false;
+  step2_income_exempt:boolean = false;
+  step2_income_agri:boolean = false;
+  step2_income_dependent:boolean = false;
+  step2_income_pf:boolean = false;
+
+  step3_house_prop_upload :boolean = true;
+  step3_prop_address:boolean = false;
+  step3_rental_prop:boolean = false;
+  step3_interest_house_loan:boolean = false;
+  step3_prop_ownership:boolean = false;
+  step3_income_dependent:boolean = false;
+  step3_unrealized_rent:boolean = false;
+
 
   constructor( 
     private scriptservice : ScriptService
@@ -18,6 +42,249 @@ export class IncomeSourceComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  //Function Called on next button click
+  on_next_click(){
+    //this.loading = true;
+    if (this.step4 == true) {
+      /*this.assSubmitted = true;
+      console.log("Assests details submitted");
+      if (this.assestsDetailsForm.invalid) {
+        return;
+      }else{
+        this.onSubmit(this.assestsDetailsForm,'asstinfo');*/
+        this.step1 = false;
+        this.step2 = false;
+        this.step3 = false;
+        this.step4 = true;
+      //}
+      
+    }
+    if (this.step3 == true) {
+      /*this.bankSubmitted = true;
+      console.log("Bank details submitted");
+      if (this.bankDetailsForm.invalid) {
+        return;
+      }else{
+        this.onSubmit(this.bankDetailsForm,'bankinfo');*/
+        this.step1 = false;
+        this.step2 = false;
+        this.step3 = false;
+        this.step4 = true;
+      //}
+      
+    }
+    if (this.step2 == true) {
+      /*this.addSubmitted = true;
+      console.log("Address details submitted");
+      if (this.addressDetailsForm.invalid) {
+        return;
+      }else{
+        this.onSubmit(this.addressDetailsForm,'addressinfo');*/
+        this.step1 = false;
+        this.step2 = false;
+        this.step3 = true;
+        this.step4 = false;
+      //}
+      
+    }
+    if (this.step1 == true) {
+      //this.perSubmitted = true;
+      console.log("Personal details submitted");
+      // stop here if form is invalid
+      /*if (this.personalDetailsForm.invalid) {
+        return;
+      }else{
+        this.onSubmit(this.personalDetailsForm,'personalinfo');*/
+        this.step1 = false;
+        this.step2 = true;
+        this.step3 = false;
+        this.step4 = false;
+      //}
+    }
+  }
+
+  //Function Called on previous button click
+  on_previous_click(){
+    if (this.step1 == true) {
+      this.step1 = true;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = false;
+    }
+    if (this.step2 == true) {
+      this.step1 = true;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = false;
+    }
+    if (this.step3 == true) {
+      this.step1 = false;
+      this.step2 = true;
+      this.step3 = false;
+      this.step4 = false;
+    }
+    if (this.step4 == true) {
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = true;
+      this.step4 = false;
+    }
+  }
+
+  //Function to enable forms from naviagtion icons
+  select_form_step(a){
+    if(a == 'step1'){
+      this.step1 = true;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = false;
+    }
+    if(a == 'step2'){
+      this.step1 = false;
+      this.step2 = true;
+      this.step3 = false;
+      this.step4 = false;
+    }
+    if(a == 'step3'){
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = true;
+      this.step4 = false;
+    }
+    if(a == 'step4'){
+      this.step1 = false;
+      this.step2 = false;
+      this.step3 = false;
+      this.step4 = true;
+    }
+  }
+
+  select_step1_subpart_form(x){
+    if(x == 'income_salary_upload'){
+      this.step1_income_salary_upload = true;
+      this.step1_income_from_salary = false;
+    }
+    if(x == 'income_from_salary'){
+      this.step1_income_salary_upload = false;
+      this.step1_income_from_salary = true;
+    }
+  }
+  select_step2_subpart_form(x){
+    if(x == 'income_other_upload'){
+      this.step2_income_other_upload = true;
+      this.step2_income_interest = false;
+      this.step2_income_others = false;
+      this.step2_income_exempt = false;
+      this.step2_income_agri = false;
+      this.step2_income_dependent = false;
+      this.step2_income_pf = false;
+    }
+    if(x == 'income_interest'){
+      this.step2_income_other_upload = false;
+      this.step2_income_interest = true;
+      this.step2_income_others = false;
+      this.step2_income_exempt = false;
+      this.step2_income_agri = false;
+      this.step2_income_dependent = false;
+      this.step2_income_pf = false;
+    }
+    if(x == 'income_others'){
+      this.step2_income_other_upload = false;
+      this.step2_income_interest = false;
+      this.step2_income_others = true;
+      this.step2_income_exempt = false;
+      this.step2_income_agri = false;
+      this.step2_income_dependent = false;
+      this.step2_income_pf = false;
+    }
+    if(x == 'income_exempt'){
+      this.step2_income_other_upload = false;
+      this.step2_income_interest = false;
+      this.step2_income_others = false;
+      this.step2_income_exempt = true;
+      this.step2_income_agri = false;
+      this.step2_income_dependent = false;
+      this.step2_income_pf = false;
+    }
+    if(x == 'income_agri'){
+      this.step2_income_other_upload = false;
+      this.step2_income_interest = false;
+      this.step2_income_others = false;
+      this.step2_income_exempt = false;
+      this.step2_income_agri = true;
+      this.step2_income_dependent = false;
+      this.step2_income_pf = false;
+    }
+    if(x == 'income_dependent'){
+      this.step2_income_other_upload = false;
+      this.step2_income_interest = false;
+      this.step2_income_others = false;
+      this.step2_income_exempt = false;
+      this.step2_income_agri = false;
+      this.step2_income_dependent = true;
+      this.step2_income_pf = false;
+    }
+    if(x == 'income_pf'){
+      this.step2_income_other_upload = false;
+      this.step2_income_interest = false;
+      this.step2_income_others = false;
+      this.step2_income_exempt = false;
+      this.step2_income_agri = false;
+      this.step2_income_dependent = false;
+      this.step2_income_pf = true;
+    }
+  }
+  select_step3_subpart_form(x){
+    if(x == 'house_prop_upload'){
+      this.step3_house_prop_upload = true;
+      this.step3_prop_address = false;
+      this.step3_rental_prop = false;
+      this.step3_interest_house_loan  = false;
+      this.step3_prop_ownership = false;
+      this.step3_unrealized_rent= false;
+    }
+    if(x == 'prop_address'){
+      this.step3_house_prop_upload = false;
+      this.step3_prop_address = true;
+      this.step3_rental_prop = false;
+      this.step3_interest_house_loan  = false;
+      this.step3_prop_ownership = false;
+      this.step3_unrealized_rent= false;
+    }
+    if(x == 'rental_prop'){
+      this.step3_house_prop_upload = false;
+      this.step3_prop_address = false;
+      this.step3_rental_prop = true;
+      this.step3_interest_house_loan  = false;
+      this.step3_prop_ownership = false;
+      this.step3_unrealized_rent= false;
+    }
+    if(x == 'interest_house_loan'){
+      this.step3_house_prop_upload = false;
+      this.step3_prop_address = false;
+      this.step3_rental_prop = false;
+      this.step3_interest_house_loan  = true;
+      this.step3_prop_ownership = false;
+      this.step3_unrealized_rent= false;
+    }
+    if(x == 'prop_ownership'){
+      this.step3_house_prop_upload = false;
+      this.step3_prop_address = false;
+      this.step3_rental_prop = false;
+      this.step3_interest_house_loan  = false;
+      this.step3_prop_ownership = true;
+      this.step3_unrealized_rent= false;
+    }
+    if(x == 'unrealized_rent'){
+      this.step3_house_prop_upload = false;
+      this.step3_prop_address = false;
+      this.step3_rental_prop = false;
+      this.step3_interest_house_loan  = false;
+      this.step3_prop_ownership = false;
+      this.step3_unrealized_rent= true;
+    }
   }
 
 }
