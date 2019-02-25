a40cf2c053db91700b20db2ee64dd0cba2ac1771
Vivek Shukla <viveksh970@IN.nam.ad.pwcinternal.com>
2019-02-25 14:37:13 +0530

    Resolved index.html and server js issue for deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,14 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+/* export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api'
+}; */
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://eazytaxin.herokuapp.com/api'
 };
 
 /*
