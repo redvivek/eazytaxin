78616238cd036b705292320161f843ac4ad31956
vivek shukla <vivekshukla@viveks-MacBook-Air.local>
2019-06-22 18:46:04 +0530

    prod env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //apiUrl: 'https://eazytaxin.herokuapp.com/api',
-  apiUrl : 'http://www.easytaxin.com/api',
+  apiUrl : 'https://www.easytaxin.com/api',
   razorAPIKey : 'rzp_live_DUvaiGWS3XUS9B',
   razorMerchantNm:'Easy Tax'
 };
